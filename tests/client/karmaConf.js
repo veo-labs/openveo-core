874503716d93ac44973563048e29d2579b22da02
Maxime Béguin <maxime.beguin@vodalys-labs.com>
2015-06-30 16:19:37 +0200

    Adapt client tests to the new sass / grunt / compass structure

--- tests/client/karmaConf.js
+++ tests/client/karmaConf.js
@@ -12,16 +12,26 @@ module.exports = function(config){
 
     // List of files / patterns to load in the browser
     files: [
-        "public/lib/angular/angular.js",
-        "public/lib/angular-animate/angular-animate.js",
-        "public/lib/angular-route/angular-route.js",
-        "public/lib/angular-cookies/angular-cookies.js",
-        "public/lib/angular-mocks/angular-mocks.js",
-        "public/js/authentication/AuthenticationApp.js",
-        "public/js/route/RouteApp.js",
-        "public/js/ov/OvApp.js",
-        "public/js/**/*.js",
-        "tests/client/unitTests/*.js"
+      "public/lib/jquery/dist/jquery.js",
+      "public/lib/datatables/media/js/jquery.dataTables.js",
+      "public/lib/api-check/dist/api-check.js",
+      "public/lib/angular/angular.js",
+      "public/lib/angular-animate/angular-animate.js",
+      "public/lib/angular-route/angular-route.js",
+      "public/lib/angular-cookies/angular-cookies.js",
+      "public/lib/angular-mocks/angular-mocks.js",
+      "public/lib/angular-touch/angular-touch.js",
+      "public/lib/angular-datatables/dist/angular-datatables.js",
+      "public/lib/angular-bootstrap/ui-bootstrap-tpls.js",
+      "public/lib/angular-ui-tree/dist/angular-ui-tree.js",
+      "public/lib/angular-formly/dist/formly.js",
+      "public/lib/multirange/multirange.js",
+      "public/lib/ng-jsonpath/dist/ng-jsonpath.js",
+      "app/client/assets/js/authentication/AuthenticationApp.js",
+      "app/client/assets/js/route/RouteApp.js",
+      "app/client/assets/js/ov/OvApp.js",
+      "app/client/assets/js/**/*.js",
+      "tests/client/unitTests/*.js"
     ],
 
     // Web server port
