efa6550dbc0d4bb8df0ca8d71ab8006fdcbe115f
Patrick Champclos <patrick.champclos@veo-labs.com>
2015-07-20 15:07:39 +0200

    SANDBOX moodle

--- script/sandbox.sh
+++ script/sandbox.sh
@@ -1,11 +1,13 @@
+# OPENVEO ******************************* OPENVEO
+echo "*********** Launch openveo sandbox" 
 SER1=rs1/127.0.0.1:27017
 SER2=rs1/127.0.0.1:27018
 SER3=rs1/127.0.0.1:27019
 DUMPOVDIR=/home/voldalys/bkp/db
 VIDEOOVDIR=/home/vodalys/bkp/video
 
-#kill server
-echo "*********** kill server" 
+#kill NodeJs server
+echo "*********** kill NodeJs server" 
 sudo killall -9 node
 
 #Restore mongodump
@@ -22,3 +24,31 @@ cp -R $VIDEOOVDIR shared/videos
 echo "*********** Restart server" 
 cd /home/vodalys/livraison/openveo/current
 NODE_ENV="production" && /usr/bin/node server.js
+
+
+# MOODLES ******************************* MOODLES
+echo "*********** Launch moodles sandbox" 
+MYSQLUSER = 'root'
+MYSQLPWD = 'root'
+MYSQLDBNAME = 'moodles'
+SQLPATHFILE = /home/vodalys/bkp/moodles/moodles.sql
+MOODLEDATAPATH = /home/vodalys/bkp/moodles/moodledata-2.9/
+
+#kill apache2 server
+echo "*********** kill apache2 server" 
+sudo service apache2 stop
+
+#Restore Mysql
+echo "*********** Restore Mysql" 
+mysql -u MYSQLUSER -p MYSQLPWD  MYSQLDBNAME < SQLPATHFILE
+
+#Remove all unused video file
+echo "*********** Remove unused file" 
+cd /home/vodalys
+rm -Rf moodledata-2.9
+cp -R $MOODLEDATAPATH moodledata-2.9 
+
+#Restart server
+echo "*********** Restart server" 
+sudo service apache2 start
+
