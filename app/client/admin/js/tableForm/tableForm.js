105026fa4c027f1230b356138b583177af25169e
Patrick Champclos <patrick.champclos@veo-labs.com>
2015-07-28 17:33:45 +0200

    Update tableForm module to add subobject key view in table, set up init function for form, add condition to toogle row and add filter to view category, date and status label

--- app/client/admin/js/tableForm/tableForm.js
+++ app/client/admin/js/tableForm/tableForm.js
@@ -2,19 +2,50 @@
 
   'use strict';
 
-  var app = angular.module("ov.tableForm", []);
+  var app = angular.module("ov.tableForm", ["ov.i18n", "ngSanitize"]);
 
   app.controller("DataTableController", DataTableController);
   app.controller("FormEditController", FormEditController);
   app.controller("FormAddController", FormAddController);
   app.factory("tableReloadEventService", TableReloadEventService);
+  app.filter('status',StatusFilter);
+  app.filter('category',CategoryFilter);
 
+  // Controller for table, form in table and form outside table
   DataTableController.$inject = ["$scope", "entityService", "$filter"];
   FormEditController.$inject = ["$scope", "$filter"];
-  FormAddController.$inject = ["$scope", "tableReloadEventService"];
+  FormAddController.$inject = ["$scope", "$filter", "tableReloadEventService"];
 
+  // Service to reload a displayed table
   TableReloadEventService.$inject = ["$rootScope"];
+  
+  // Filter to display content in the table (cf. dataTable.html)
+  StatusFilter.$inject = ['$filter'];
+  CategoryFilter.$inject = ['jsonPath'];
+  
+  function StatusFilter($filter){
+    return function (input, status, errorCode) {
+      var type = 'label-danger';
+      if(status == 1){
+        if(input == 7) type = 'label-success';
+        if(input == 6) type = 'label-warning';
+      } else if (status == 2){
+        type = 'label-info';
+      }
+      var label = $filter('translate')('VIDEOS.STATE_' + input);
+      if(!status) label = label +'('+errorCode+')';
+      return "<span class='label "+type+"'>"+label+"</span>";
+    };
+  };
 
+  function CategoryFilter(jsonPath){
+    return function(input, rubrics) {
+      var name = jsonPath(rubrics, '$..*[?(@.id=="'+input+'")].title');
+      if (name && name.length>0)  return name[0];
+      else return "";
+    };
+  };
+  
 /**
  * 
  * Service reload Table
@@ -34,9 +65,12 @@
    */
   function FormEditController($scope, $filter) {
     var fec = this;
-    fec.model = $scope.editFormContainer.model;
-    fec.originalFields = angular.copy(fec.fields);
-    fec.fields = $scope.editFormContainer.fields;
+    fec.init = function(row){
+      fec.model = row;
+      if($scope.editFormContainer.init) $scope.editFormContainer.init(row) ;
+      fec.fields = $scope.editFormContainer.fields;
+      fec.originalFields = angular.copy(fec.fields);
+    };
     
     fec.onSubmit = function () {
       $scope.editFormContainer.onSubmit(fec.model, function () {
@@ -55,7 +89,7 @@
    * FormController
    *  
    */
-  function FormAddController($scope, tableReloadEventService, $filter) {
+  function FormAddController($scope, $filter, tableReloadEventService) {
     var vm = this;
     vm.model = $scope.addFormContainer.model;
     vm.originalFields = angular.copy(vm.fields);
@@ -81,10 +115,13 @@
    */
   function DataTableController($scope, entityService) {
     var dataTable = this;
+    //init value
     dataTable.rows = {};
+    dataTable.entityType = $scope.tableContainer.entityType || "";
+    dataTable.conditionTogleDetail = $scope.tableContainer.conditionTogleDetail || function(val){return true};
     dataTable.filterBy = angular.copy($scope.tableContainer.filterBy);
-    dataTable.header = $scope.tableContainer.header;
-    dataTable.actions = $scope.tableContainer.actions;
+    dataTable.header = $scope.tableContainer.header || [];
+    dataTable.actions = $scope.tableContainer.actions || [];
     dataTable.notSortBy = ["action"];
     
     dataTable.init = {
@@ -111,10 +148,11 @@
       param['sort'][paramsObj.sortBy] = paramsObj.sortOrder == "dsc" ? -1 : 1;
       param['filter'] = {};
       for (var key in dataTable.filterBy) {
-        param['filter'][key] = {"$regex": ".*" + dataTable.filterBy[key] + ".*"}
+        if ( dataTable.filterBy[key] != "")
+          param['filter'][key] = {"$regex": ".*" + dataTable.filterBy[key] + ".*"}
       }
 
-      return entityService.getEntities('property', param).then(function (response) {
+      return entityService.getEntities(dataTable.entityType, param).then(function (response) {
         dataTable.rows = response.data.rows;
 
         return {
@@ -126,8 +164,9 @@
         }
       });
     }
-    dataTable.toggleRowDetails = function (row) {
-      if (!row.saving) {
+    dataTable.toggleRowDetails = function (row, condition) {
+      
+      if (!row.saving && condition) {
         angular.forEach(dataTable.rows, function (value, key) {
           value.opened = (value.id === row.id) ? !value.opened : false;
         })
@@ -139,6 +178,12 @@
     $scope.$on('reloadDataTable', function() {
         dataTable.reloadCallback();
     });
+    
+    dataTable.getDescendantProp = function(obj, desc) {
+      var arr = desc.split(".");
+      while(arr.length && (obj = obj[arr.shift()]));
+      return obj;
+    }
   }
 
 })(angular);
