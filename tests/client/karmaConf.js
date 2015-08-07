c0610172ef060b4f654d6a102cbe3b3f9312be69
Patrick Champclos <patrick.champclos@veo-labs.com>
2015-08-07 18:01:19 +0200

    Update KarmaConf

--- tests/client/karmaConf.js
+++ tests/client/karmaConf.js
@@ -9,21 +9,24 @@ module.exports = function(config){
 
     // List of files / patterns to load in the browser
     files: [
-      "public/lib/jquery/dist/jquery.js",
-      "public/lib/datatables/media/js/jquery.dataTables.js",
       "public/lib/api-check/dist/api-check.js",
       "public/lib/angular/angular.js",
       "public/lib/angular-animate/angular-animate.js",
       "public/lib/angular-route/angular-route.js",
       "public/lib/angular-cookies/angular-cookies.js",
+      "public/lib/angular-bootstrap/ui-bootstrap-tpls.js",
       "public/lib/angular-mocks/angular-mocks.js",
       "public/lib/angular-touch/angular-touch.js",
-      "public/lib/angular-datatables/dist/angular-datatables.js",
-      "public/lib/angular-bootstrap/ui-bootstrap-tpls.js",
+      "public/lib/angular-sanitize/angular-sanitize.js",
       "public/lib/angular-ui-tree/dist/angular-ui-tree.js",
       "public/lib/angular-formly/dist/formly.js",
+      "public/lib/angular-formly-templates-bootstrap/dist/angular-formly-templates-bootstrap.js",
+      "public/lib/angular-xeditable/dist/js/xeditable.js",
+      "public/lib/checklist-model/checklist-model.js",
       "public/lib/multirange/multirange.js",
       "public/lib/ng-jsonpath/dist/ng-jsonpath.js",
+      "public/lib/ng-tasty/ng-tasty-tpls.js",
+      
       "app/client/admin/js/authentication/AuthenticationApp.js",
       "app/client/admin/js/route/RouteApp.js",
       "app/client/admin/js/ov/OvApp.js",
