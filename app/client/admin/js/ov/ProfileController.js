5332c40788fcf6f5f1dba58040ab05b83a6514aa
AJE <azza.jedidi@veo-labs.com>
2015-09-28 12:03:41 +0200

    OpenVeo 0000007 : User profile : password edition

--- app/client/admin/js/ov/ProfileController.js
+++ app/client/admin/js/ov/ProfileController.js
@@ -39,6 +39,9 @@
       }).success(function() {
         userInfo.saving = false;
         $scope.$emit('setAlert', 'success', $filter('translate')('UI.SAVE_SUCCESS'), 4000);
+        $scope.password = '';
+        $scope.confirmPassword = '';
+        $scope.isInValid = true; 
       }).error(function(data, status) {
         userInfo.saving = false;
         $scope.$emit('setAlert', 'danger', $filter('translate')('UI.SAVE_ERROR'), 4000);
