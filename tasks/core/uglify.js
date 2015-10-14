619966c94aba3ee0962701f85344c0fe1f78cb06
Nicolas Le Boudouil <nicolas.leboudouil@veo-labs.com>
2015-10-14 10:04:22 +0200

    missing configuration file for last fix 000009

--- tasks/core/uglify.js
+++ tasks/core/uglify.js
@@ -14,7 +14,7 @@ module.exports = {
       {
         expand: true, // Enable dynamic expansion.
         cwd: '<%= project.beJS %>/', // Src matches are relative to this path.
-        src: ['ov/*.js'], // Actual pattern(s) to match.
+        src: ['*.js', 'ov/*.js'], // Actual pattern(s) to match.
         dest: '<%= project.uglify %>/', // Destination path prefix.
         ext: '.min.js', // Dest filepaths will have this extension.
         extDot: 'first'   // Extensions in filenames begin after the first dot
