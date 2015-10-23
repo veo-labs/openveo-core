bb1a2d0e921f792e4f60a3f78f47b81630519484
Maxime Béguin <maxime.beguin@vodalys-labs.com>
2015-10-23 17:49:19 +0200

    Add configuration files generation to installation process

--- install.js
+++ install.js
@@ -4,7 +4,14 @@
 var readline = require('readline');
 var crypto = require('crypto');
 var path = require('path');
+var fs = require('fs');
+var os = require('os');
+var async = require('async');
 var openVeoAPI = require('@openveo/api');
+var confDir = path.join(openVeoAPI.fileSystem.getConfDir(), 'core');
+var exit = process.exit;
+var conf;
+var databaseConf;
 
 // Set module root directory and define a custom require function
 process.root = __dirname;
@@ -13,110 +20,311 @@ process.require = function(filePath) {
 };
 
 var UserProvider = process.require('app/server/providers/UserProvider.js');
-var conf = process.require('config/conf.json');
-var databaseConf = process.require('config/databaseConf.json');
 
-var countLines = 1;
-var superAdminName;
-var superAdminPassword;
-var superAdminEmail;
-var exit = process.exit;
-
-// Get a Database instance
-var db = openVeoAPI.Database.getDatabase(databaseConf);
+// Create a readline interface to interact with the user
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 /**
- * Adds a user in the database.
+ * Creates a random hash.
  *
- * @param {Object} data User information
- * @param {Function} callback Function to call when its done
+ * @param {Number} size The length of the expected hash
+ * @return {String} A hash
  */
-function addUser(data, callback) {
-  var userProvider = new UserProvider(openVeoAPI.applicationStorage.getDatabase());
+function getRandomHash(size) {
+  return crypto.randomBytes(256).toString('base64').slice(0, size);
+}
 
-  // Verify if the super admin does not exist
-  userProvider.getOne(0, function(error, user) {
-    if (user) {
-      callback(new Error('A super admin user already exists'));
-    } else if (error) {
-      callback(error);
+/**
+ * Creates conf directory if it does not exist.
+ */
+function createConfDir(callback) {
+  fs.exists(confDir, function(exists) {
+    if (exists)
+      callback();
+    else
+      openVeoAPI.fileSystem.mkdir(confDir, callback);
+  });
+}
+
+/**
+ * Creates general configuration file if it does not exist.
+ */
+function createConf(callback) {
+  var confFile = path.join(confDir, 'conf.json');
+
+  fs.exists(confFile, function(exists) {
+    if (exists) {
+      process.stdout.write(confFile + ' already exists\n');
+      callback();
     } else {
-      userProvider.add(data, function(error) {
-        callback(error);
+      var hash = getRandomHash(10);
+      rl.question('Enter a secret key used to encrypt users passwords (default: ' + hash + ') :\n', function(answer) {
+        var conf = {
+          passwordHashKey: answer || hash
+        };
+
+        fs.writeFile(confFile, JSON.stringify(conf, null, '\t'), {encoding: 'utf8'}, callback);
       });
     }
   });
 }
 
-// Establish connection to the database
-db.connect(function(error) {
-  if (error) {
-    process.stdout.write('Could not connect to the database with message : ' + error.message);
-    exit();
-  }
+/**
+ * Creates database configuration file if it does not exist.
+ */
+function createDatabaseConf(callback) {
+  var confFile = path.join(confDir, 'databaseConf.json');
+  var conf = {
+    type: 'mongodb',
+    host: 'localhost',
+    port: 27017
+  };
 
-  openVeoAPI.applicationStorage.setDatabase(db);
-
-  // Create a readline interface to interact with the user
-  var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  async.series([
+    function(callback) {
+      fs.exists(confFile, function(exists) {
+        if (exists)
+          callback(new Error(confFile + ' already exists\n'));
+        else
+          callback();
+      });
+    },
+    function(callback) {
+      rl.question('Enter database host (default: ' + conf.host + ') :\n', function(answer) {
+        conf.host = answer || conf.host;
+        callback();
+      });
+    },
+    function(callback) {
+      rl.question('Enter database port (default: ' + conf.port + ') :\n', function(answer) {
+        conf.port = answer || conf.port;
+        callback();
+      });
+    },
+    function(callback) {
+      rl.question('Enter database name :\n', function(answer) {
+        conf.database = answer || '';
+        callback();
+      });
+    },
+    function(callback) {
+      rl.question('Enter database user name :\n', function(answer) {
+        conf.username = answer || '';
+        callback();
+      });
+    },
+    function(callback) {
+      rl.question('Enter database user password :\n', function(answer) {
+        conf.password = answer || '';
+        callback();
+      });
+    }
+  ], function(error, results) {
+    if (error) {
+      process.stdout.write(error.message);
+      callback();
+    }
+    else
+      fs.writeFile(confFile, JSON.stringify(conf, null, '\t'), {encoding: 'utf8'}, callback);
   });
+}
 
-  process.stdout.write('Enter the name of the OpenVeo super admin to create :\n');
-  rl.prompt();
+/**
+ * Creates loggers configuration file if it does not exist.
+ */
+function createLoggerConf(callback) {
+  var confFile = path.join(confDir, 'loggerConf.json');
+  var conf = {
+    app: {
+      level: 'info',
+      maxFileSize: 1048576,
+      maxFiles: 2
+    },
+    ws: {
+      level: 'info',
+      maxFileSize: 1048576,
+      maxFiles: 2
+    }
+  };
 
-  // Listen to user answers
-  rl.on('line', function(line) {
-    line = line.trim();
+  async.series([
+    function(callback) {
+      fs.exists(confFile, function(exists) {
+        if (exists)
+          callback(new Error(confFile + ' already exists\n'));
+        else
+          callback();
+      });
+    },
+    function(callback) {
+      var defaultFilePath = path.join(os.tmpdir(), 'logs', 'openveo', 'openveo.log');
+      rl.question('Enter OpenVeo log file path (default: ' + defaultFilePath + ') :\n', function(answer) {
+        conf.app.fileName = answer || defaultFilePath;
+        callback();
+      });
+    },
+    function(callback) {
+      var defaultFilePath = path.join(os.tmpdir(), 'logs', 'openveo', 'openveo-ws.log');
+      rl.question('Enter OpenVeo Web Service log file path (default: ' + defaultFilePath + ') :\n', function(answer) {
+        conf.ws.fileName = answer || defaultFilePath;
+        callback();
+      });
+    }
+  ], function(error, results) {
+    if (error) {
+      process.stdout.write(error.message);
+      callback();
+    }
+    else
+      fs.writeFile(confFile, JSON.stringify(conf, null, '\t'), {encoding: 'utf8'}, callback);
+  });
+}
 
-    try {
+/**
+ * Creates server configuration file if it does not exist.
+ */
+function createServerConf(callback) {
+  var confFile = path.join(confDir, 'serverConf.json');
+  var conf = {
+    app: {
+      port: 3000
+    },
+    ws: {
+      port: 3001
+    }
+  };
 
-      switch (countLines) {
-        case 1:
-          if (!line) throw new Error('Invalid name, aborting');
-          superAdminName = line;
-          process.stdout.write('Enter the password of the OpenVeo super admin to create :\n');
-          rl.prompt();
-          break;
-        case 2:
-          if (!line) throw new Error('Invalid password, aborting');
-          superAdminPassword = crypto.createHmac('sha256', conf.passwordHashKey).update(line).digest('hex');
-          process.stdout.write('Enter the email of the OpenVeo super admin to create :\n');
-          rl.prompt();
-          break;
-        case 3:
-          if (!line || !openVeoAPI.util.isEmailValid(line)) throw new Error('Invalid email, aborting');
-          superAdminEmail = line;
-          addUser({
-            id: 0,
-            name: superAdminName,
-            email: superAdminEmail,
-            password: superAdminPassword,
-            locked: true
-          }, function(error) {
-            if (error) {
-              process.stdout.write('Couldn\'t create the super admin user due to the following error : ' +
-                                    error.message);
-            } else {
-              process.stdout.write('Super admin ' + superAdminName + ' (' + superAdminEmail + ') created');
-            }
+  async.series([
+    function(callback) {
+      fs.exists(confFile, function(exists) {
+        if (exists)
+          callback(new Error(confFile + ' already exists\n'));
+        else
+          callback();
+      });
+    },
+    function(callback) {
+      var hash = getRandomHash(40);
+      rl.question('Enter a hash to secure HTTP sessions (default: ' + hash + ') :\n', function(answer) {
+        conf.app.sessionSecret = answer || hash;
+        callback();
+      });
+    },
+    function(callback) {
+      rl.question('Enter OpenVeo server port (default: ' + conf.app.port + ') :\n', function(answer) {
+        conf.app.port = answer || conf.app.port;
+        callback();
+      });
+    },
+    function(callback) {
+      rl.question('Enter OpenVeo Web Service server port (default: ' + conf.ws.port + ') :\n', function(answer) {
+        conf.ws.port = answer || conf.ws.port;
+        callback();
+      });
+    }
+  ], function(error, results) {
+    if (error) {
+      process.stdout.write(error.message);
+      callback();
+    }
+    else
+      fs.writeFile(confFile, JSON.stringify(conf, null, '\t'), {encoding: 'utf8'}, callback);
+  });
+}
 
-            rl.close();
-            exit();
-          });
-          break;
-        default:
-          rl.close();
-          exit();
-      }
+/**
+ * Verifies connection to the database.
+ */
+function verifyDatbaseConf(callback) {
+  databaseConf = require(path.join(confDir, 'databaseConf.json'));
+  conf = require(path.join(confDir, 'conf.json'));
 
-    } catch (e) {
-      process.stdout.write(e.message);
+  var db = openVeoAPI.Database.getDatabase(databaseConf);
+
+  db.connect(function(error) {
+    if (error) {
+      process.stdout.write('Could not connect to the database with message : ' + error.message);
       exit();
     }
 
-    countLines++;
+    openVeoAPI.applicationStorage.setDatabase(db);
+    callback();
   });
+}
 
+/**
+ * Creates super administrator if it does not exist.
+ */
+function createSuperAdmin(callback) {
+  var userProvider = new UserProvider(openVeoAPI.applicationStorage.getDatabase());
+  var user = {
+    id: 0,
+    locked: true
+  };
+
+  async.series([
+    function(callback) {
+
+      // Verify if the super admin does not exist
+      userProvider.getOne(0, function(error, user) {
+        if (user)
+          callback(new Error('A super admin user already exists\n'));
+        else if (error)
+          callback(error);
+        else
+          callback();
+      });
+    },
+    function(callback) {
+      rl.question('Enter the name of the OpenVeo super admin to create :\n', function(answer) {
+        if (!answer) callback(new Error('Invalid name, aborting'));
+        user.name = answer;
+        callback();
+      });
+    },
+    function(callback) {
+      rl.question('Enter the password of the OpenVeo super admin to create :\n', function(answer) {
+        if (!answer) callback(new Error('Invalid password, aborting'));
+        user.password = crypto.createHmac('sha256', conf.passwordHashKey).update(answer).digest('hex');
+        callback();
+      });
+    },
+    function(callback) {
+      rl.question('Enter the email of the OpenVeo super admin to create :\n', function(answer) {
+        if (!answer || !openVeoAPI.util.isEmailValid(answer)) callback(Error('Invalid email, aborting'));
+        user.email = answer;
+        callback();
+      });
+    }
+  ], function(error, results) {
+    if (error) {
+      process.stdout.write(error.message);
+      callback();
+    }
+    else
+      userProvider.add(user, function(error) {
+        callback(error);
+      });
+  });
+}
+
+// Launch installation
+async.series([
+  createConfDir,
+  createConf,
+  createDatabaseConf,
+  createLoggerConf,
+  createServerConf,
+  verifyDatbaseConf,
+  createSuperAdmin
+], function(error, results) {
+  if (error)
+    throw error;
+  else {
+    process.stdout.write('Installation complete');
+    exit();
+  }
 });
