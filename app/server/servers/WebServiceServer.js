efb074039d7ade3cfcb49678486a390cb6471b47
Maxime Béguin <maxime.beguin@vodalys-labs.com>
2016-01-28 17:17:50 +0100

    Inform caches not to cache get requests
    All get requests were potentially cached by user agents. Get requests are no longer cached.

--- app/server/servers/WebServiceServer.js
+++ app/server/servers/WebServiceServer.js
@@ -55,6 +55,21 @@ function WebServiceServer() {
   this.router.all('*', oAuth.middleware.bearer);
   this.router.all('*', oAuthController.validateScopesAction);
 
+  // Deactivate user agent cache for all get requests
+  // Cache-Control : no-cache to force caches to request the original server
+  // Cache-Control : no-store to force caches not to keep any copy of the response
+  // Cache-Control : must-revalidate to force caches to ask original server validation of a stale response
+  // Pragma : no-cache to be backward compatible with HTTP/1.0 caches
+  // Expires : 0 to mark all responses as staled
+  this.app.get('*', function(request, response, next) {
+    response.set({
+      'Cache-Control': 'no-cache, no-store, must-revalidate',
+      Pragma: 'no-cache',
+      Expires: 0
+    });
+    next();
+  });
+
   // Mount router
   this.app.use('/', this.router);
 }
