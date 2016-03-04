d3d2d03bccb6a2ca053cf0cdf797a2ee9586ac4f
Maxime Béguin <maxime.beguin@vodalys-labs.com>
2016-03-04 17:34:45 +0100

    Deactivate oauth20-provider logs when process logs console is not activated

--- app/server/oauth/oAuth.js
+++ app/server/oauth/oAuth.js
@@ -8,7 +8,7 @@ var OAuthLib = require('oauth20-provider');
 var client = process.require('app/server/oauth/client.js');
 var accessToken = process.require('app/server/oauth/accessToken.js');
 
-var oAuth = new OAuthLib();
+var oAuth = new OAuthLib({log: {level: (process.logger.transports.console) ? 0 : 5}});
 oAuth.model.client.getId = client.getId;
 oAuth.model.client.fetchById = client.fetchById;
 oAuth.model.client.checkSecret = client.checkSecret;
