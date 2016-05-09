f2319cbf4f4b3a4b7fe3be8ff2dfa47ffc38d792
Anthony FERTIL <anthony.fertil@veo-labs.com>
2016-05-09 14:08:40 +0200

    Update migration for the permissions names

--- migrations/2.0.0.js
+++ migrations/2.0.0.js
@@ -51,12 +51,96 @@ module.exports.update = function(callback) {
 
         else {
             db.insert('core_sessions', value, function(error) {
-                callback(error);
+                if (error) {
+                    callback(error);
+                    return;
+                }
             });
             db.removeCollection('sessions', function(error) {
-                callback(error);
+                if (error) {
+                    callback(error);
+                    return;
+                }
             });
         }
+    });
+
+    // Rename permissions names
+    db.get('core_roles', {}, null, null, function(error, value) {
+        if (error) {
+            callback(error);
+            return;
+        }
+
+        // No need to change anything
+        if (!value || !value.length) callback();
+
+        var permissions = [];
+
+        value.forEach(function(role) {
+            if (role.permissions) {
+                permissions = [];
+                role['permissions'].forEach(function(permission) {
+                    switch(permission) {
+                        case 'create-application':
+                            permissions.push('core-add-applications');
+                            break;
+                        case 'update-application':
+                            permissions.push('core-update-applications');
+                            break;
+                        case 'delete-application':
+                            permissions.push('core-delete-applications');
+                            break;
+                        case 'create-taxonomy':
+                            permissions.push('core-add-taxonomies');
+                            break;
+                        case 'update-taxonomy':
+                            permissions.push('core-update-taxonomies');
+                            break;
+                        case 'delete-taxonomy':
+                            permissions.push('core-delete-taxonomies');
+                            break;
+                        case 'create-user':
+                            permissions.push('core-add-users');
+                            break;
+                        case 'update-user':
+                            permissions.push('core-update-users');
+                            break;
+                        case 'delete-user':
+                            permissions.push('core-delete-users');
+                            break;
+                        case 'create-role':
+                            permissions.push('core-add-roles');
+                            break;
+                        case 'update-role':
+                            permissions.push('core-update-roles');
+                            break;
+                        case 'delete-role':
+                            permissions.push('core-delete-roles');
+                            break;
+                        case 'access-applications-page':
+                            permissions.push('core-access-applications-page');
+                            break;
+                        case 'access-users-page':
+                            permissions.push('core-access-users-page');
+                            break;
+                        case 'access-roles-page':
+                            permissions.push('core-access-roles-page');
+                            break;
+                        default:
+                            permissions.push(permission);
+                            break;
+                    }
+                });
+
+                db.update('core_roles', {id: role.id}, {permissions: permissions}, function(error) {
+                    if (error) {
+                        callback(error);
+                        return;
+                    }
+                });
+            }
+        });
     });
 
     process.logger.info('Core 2.0.0 migration done.');
