5edea5396a130e35bac37c64d3df342f2e17ad6c
Patrick Champclos <patrick.champclos@veo-labs.com>
2016-05-30 14:39:34 +0200

    Auto-generated documentation for version 2.0.0

---
+++ 2.0.0/api/back-end/api.js
@@ -0,0 +1,59 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "alertService",
+        "applicationService",
+        "authenticationService",
+        "entityService",
+        "i18nService",
+        "menuService",
+        "ovMultiCheckBox",
+        "ovTags",
+        "storageProvider",
+        "translateFilter",
+        "truncateFilter",
+        "userService"
+    ],
+    "modules": [
+        "ov",
+        "ov.alert",
+        "ov.authentication",
+        "ov.entity",
+        "ov.i18n",
+        "ov.storage"
+    ],
+    "allModules": [
+        {
+            "displayName": "ov",
+            "name": "ov",
+            "description": "Main OpenVeo module.\n\nLoads all AngularJS dependencies and plugins modules. It also initializes formly and define routes exposed\nby OpenVeo core."
+        },
+        {
+            "displayName": "ov.alert",
+            "name": "ov.alert",
+            "description": "Controls alerts for the whole application."
+        },
+        {
+            "displayName": "ov.authentication",
+            "name": "ov.authentication",
+            "description": "Service to authenticate / logout or manipulate authenticated user informations."
+        },
+        {
+            "displayName": "ov.entity",
+            "name": "ov.entity",
+            "description": "Manage OpenVeo entities described in conf.js."
+        },
+        {
+            "displayName": "ov.i18n",
+            "name": "ov.i18n",
+            "description": "Control back end internationalization."
+        },
+        {
+            "displayName": "ov.storage",
+            "name": "ov.storage",
+            "description": "Helper module to manipulate local storage."
+        }
+    ],
+    "elements": []
+} };
+});
