651d16ff10899507df86d2788de689e3b0932c03
Maxime Béguin <maxime.beguin@veo-labs.com>
2018-06-19 15:50:39 +0200

    Auto-generated documentation for version 5.1.1

---
+++ 5.1.1/api/server/api.js
@@ -0,0 +1,98 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "ApplicationController",
+        "ApplicationServer",
+        "CORE_HOOKS",
+        "ClientProvider",
+        "CorePlugin",
+        "CorePluginApi",
+        "DefaultController",
+        "ErrorController",
+        "GroupController",
+        "GroupProvider",
+        "HTTP_ERRORS",
+        "I18nController",
+        "MenuController",
+        "OauthController",
+        "RoleController",
+        "RoleProvider",
+        "Server",
+        "SettingProvider",
+        "SettingsController",
+        "TaxonomyController",
+        "TaxonomyProvider",
+        "TokenProvider",
+        "UserController",
+        "UserProvider",
+        "WebServiceServer",
+        "accessToken",
+        "api",
+        "authenticationController",
+        "authenticator",
+        "client",
+        "entityLoader",
+        "listener",
+        "migrationLoader",
+        "migrationProcess",
+        "namespaceLoader",
+        "path",
+        "permissionLoader",
+        "pluginLoader",
+        "routeLoader",
+        "storage"
+    ],
+    "modules": [
+        "core",
+        "core-controllers",
+        "core-loaders",
+        "core-migration",
+        "core-oauth",
+        "core-plugin",
+        "core-providers",
+        "core-servers"
+    ],
+    "allModules": [
+        {
+            "displayName": "core",
+            "name": "core"
+        },
+        {
+            "displayName": "core-controllers",
+            "name": "core-controllers",
+            "description": "Controllers provide all route actions."
+        },
+        {
+            "displayName": "core-loaders",
+            "name": "core-loaders",
+            "description": "Loaders module includes tools to be able to load configuration found in\ncore and plugins."
+        },
+        {
+            "displayName": "core-migration",
+            "name": "core-migration",
+            "description": "Provides functions to execute migration script."
+        },
+        {
+            "displayName": "core-oauth",
+            "name": "core-oauth",
+            "description": "Oauth 2 implementation."
+        },
+        {
+            "displayName": "core-plugin",
+            "name": "core-plugin",
+            "description": "The Core plugin with its APIs."
+        },
+        {
+            "displayName": "core-providers",
+            "name": "core-providers",
+            "description": "Providers module includes all core providers."
+        },
+        {
+            "displayName": "core-servers",
+            "name": "core-servers",
+            "description": "Servers module includes all different servers defined by openveo (HTTP and Web Socket)."
+        }
+    ],
+    "elements": []
+} };
+});
